020d73695d38c237dfe445d281caecbf1774470a
Saulo Nunes <saulo@paralink.com.br>
2025-01-31 15:11:24 -0300

    chore: update jest tests coverage

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,24 @@
-import { add } from '../src'
+import { add, start, world } from '../src'
 
 test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3)
+})
+
+test('adds -1 + -2 to equal -3', () => {
+  expect(add(-1, -2)).toBe(-3)
+})
+
+test('adds 0 + 0 to equal 0', () => {
+  expect(add(0, 0)).toBe(0)
+})
+
+test('start function logs correct message', async () => {
+  const consoleSpy = jest.spyOn(console, 'log')
+  await start('Hello')
+  expect(consoleSpy).toHaveBeenCalledWith('Hello ' + world)
+  consoleSpy.mockRestore()
+})
+
+test('world constant is "World!!!"', () => {
+  expect(world).toBe('World!!!')
 })
